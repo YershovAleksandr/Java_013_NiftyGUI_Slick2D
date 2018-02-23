3fe16df14f57fafd44e0b130cc73b5e576b7fd03
hp <namstudionsk@gmail.com>
2018-02-23 21:06:27 +0700

    Add log support

---
+++ src/com/nam/Main.java
@@ -0,0 +1,15 @@
+package com.nam;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+	// write your code here
+
+        log.info("Hello world Motherfucka!!!");
+
+    }
+}
