3344a993010c870d8785bfcb50877bfff796800c
hp <namstudionsk@gmail.com>
2018-02-23 21:17:25 +0700

    Add Slick2d support

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -3,6 +3,8 @@ package com.nam;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import org.newdawn.slick.*;
+
 public class Main {
     private static Logger log = LoggerFactory.getLogger(Main.class);
 
@@ -10,6 +12,14 @@ public class Main {
 	// write your code here
 
         log.info("Hello world Motherfucka!!!");
+
+        try {
+            AppGameContainer apgc = new AppGameContainer(new Slick2DGame("Hack42"));
+            apgc.setDisplayMode(1024, 768, false);
+            apgc.start();
+        } catch (SlickException ex){
+            log.error("Wtf", ex);
+        }
 
     }
 }
